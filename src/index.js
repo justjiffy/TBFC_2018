cb4871b21305372a919ecb140a3eeb46f3ab10f1
Juli Emmel <justjiffy@gmail.com>
2017-08-19 20:19:19 -0700

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App.jsx';
+import Button from 'react-toolbox/lib/button/';
+
+ReactDOM.render(
+  <Button label="Hello, world!" />,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './dist/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const postcss = require('postcss-cssnext');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/,
+          loaders: [
+            'style-loader?sourceMap',
+            'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+          ]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig, postcss]
+};
