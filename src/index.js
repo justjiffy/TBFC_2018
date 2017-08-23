d31d7c7a68bed466d95e4a61f17d4df2197bfc77
Jeff Leplomet <jleplomet@gmail.com>
2017-08-22 18:00:22 -0700

    first component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/App.jsx';
-import Button from 'react-toolbox/lib/button/';
+
+const nextEvent = new Date('5/27/2018');
+
+class Countdown extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(
+			() => this.tick(), 
+			1000
+		);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
+	render() {
+	console.log(this.props.date);
+		return ( 
+			<div>
+				<h1>Always the 3rd Sunday in May, {this.props.date}</h1>
+				<h2>It is {this.state.date.toDateString()}</h2>
+			</div>
+		);
+	}
+
+}
 
 ReactDOM.render(
-  <Button label="Hello, world!" />,
+  <Countdown date={nextEvent.toDateString()} />,
   document.getElementById('root')
 );
