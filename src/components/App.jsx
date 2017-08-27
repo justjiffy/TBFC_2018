28b42735fa6f1f7fac9724288e87295b1d92db4c
Juli Emmel <justjiffy@gmail.com>
2017-08-26 17:40:19 -0700

    router not working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Layout, Panel } from 'react-toolbox/lib'
+import { Switch, Route } from 'react-router-dom';
+import { Layout, NavDrawer, Panel, AppBar } from 'react-toolbox/lib';
 
 import Landing from './Landing.jsx';
+import Event from './Event.jsx';
 import Nav from './Nav.jsx';
 
 import theme from './App.css';
@@ -10,13 +12,31 @@ import theme from './App.css';
 const nextEvent = new Date('5/27/2018');
 
 export default class App extends React.Component{
+  state = {
+    drawerActive: false,
+    drawerPinned: false
+  }
+
+  toggleDrawerActive = () => {
+      this.setState({ drawerActive: !this.state.drawerActive });
+  }
+
+
   render() {
     return (
       <div>
-        <Layout theme={theme}>
-            <Nav />
-            <Panel className={theme.navPadding}>
-              <Landing date={nextEvent.toDateString()} />
+        <Layout>
+            <NavDrawer active={this.state.drawerActive}
+                pinned={this.state.drawerPinned}
+                onOverlayClick={ this.toggleDrawerActive }>
+                 <Nav />
+            </NavDrawer>
+            <Panel>
+                <AppBar rightIcon='menu' onRightIconClick={ this.toggleDrawerActive } flat />
+                  <Switch>
+                    <Route exact path='/' component={Landing}/>
+                    <Route path='/event' component={Event}/>
+                  </Switch>
             </Panel>
         </Layout>
       </div>
---
+++ src/components/Event.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Event extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>EVENT INFO</h1>
+        <p>More info coming soon</p>
+      </div>
+    );
+  }
+}
